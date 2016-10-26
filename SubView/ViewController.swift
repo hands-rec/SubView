63ec54c649c915454f0ac170e690c268e5591444
hands-rec <hands.records@gmail.com>
2016-10-26 23:48:00 +0900

    Initial Commit

---
+++ SubView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SubView
+//
+//  Created by hiro on 2016/10/26.
+//  Copyright (c) 2016年 hands-rec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
