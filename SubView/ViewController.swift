e4274881ffc17fee774d6094d3bf7ffe2ce7af8e
hands-rec <hands.records@gmail.com>
2016-10-27 00:45:25 +0900

    Add subview

--- SubView/ViewController.swift
+++ SubView/ViewController.swift
@@ -2,22 +2,44 @@
 //  ViewController.swift
 //  SubView
 //
-//  Created by hiro on 2016/10/26.
-//  Copyright (c) 2016年 hands-rec. All rights reserved.
+//  Created by hands-rec on 2016/10/26.
+//  Copyright (c) 2016 hands-rec. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var baseView: UIView!
+    
+    var subLabel: UILabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.setupLabel()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func setupLabel() {
+        subLabel.text = "テスト"
+        subLabel.backgroundColor = UIColor(red: 0.8, green: 1.0, blue: 1.0, alpha:1.0)
+        
+        let subLabelHeight: CGFloat = 100
+        let subLabelWidth: CGFloat = 100
+        let subLabelX: CGFloat = baseView.frame.width - subLabelWidth
+        let subLabelY: CGFloat = baseView.frame.height - subLabelHeight
+        subLabel.frame = CGRect(x: subLabelX, y: subLabelY, width: 100, height: subLabelHeight)
+        
+        baseView.addSubview(subLabel)
     }
 
 
